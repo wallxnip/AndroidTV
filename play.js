3d820d6d4019b5fce85c5c3c13488e7b93e0e4d3
Apps <wallxnip@gmail.com>
2025-10-29 18:27:35 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,11 +1,11 @@
 // Dados de séries e episódios (8 episódios)
 const episodes = [
-    {title:"Episódio 1", background:"bg1.jpg", thumb:"p.jpg", url:"1.mp4"},
-    {title:"Episódio 2", background:"bg1.jpg", thumb:"p.jpg", url:"2.mp4"},
-    {title:"Episódio 3", background:"bg1.jpg", thumb:"p.jpg", url:"https://download1476.mediafire.com/1k62dcn3nnggburseyTwTRUji4yT3L_D5obORNcekQLjqMPekspSkXZP3ZaGpupnnrNd8sUPpbMMljXT9-6e8jybF5KnP4oCDT3j5I-FSowIYl6etH_6RYQzmLPUMR8T2408Wgrf9wFUFkyZwSDnhchNvjbBarSCISidVVurdLsJWw/2y2yxu3x3rba1fr/3-5%287%29.mp4"},
-    {title:"Episódio 4", thumb:"p.jpg", url:"https://download1580.mediafire.com/b8hqntihryigIC6SZMVzMZ_8GUjBSBuKTty05dIkY2LfwpsdFjwvUb65UqxJ0yyz9JzbchnJ2VAiXbAi_wq3roVNhbmXOus-ER3quXclgqPJwic8h4qcPk2uoIj_jm2AmZCstvQqNxit471iFXbeM2APkjPbnxFZmGQBD7LMFQE/l2b4zlk1w38xcf6/3-6%285%29.mp4"},
-    {title:"Episódio 5", thumb:"p.jpg", url:"https://download1326.mediafire.com/umn26y7p8negENFjyCVMPL-rXGzti-2pxNfmjedHFfNLZmcbWbUtFu4j1-0Z91kHS5n-NNnGk8nyuKZdCqF1uJ7uXqbMcOnH24Py9umWwEd-LSk5a1xGdkczEETQlvcPAUW9w_x-tcsqfIZ1k3zZLUvnZHWSZhugMIyZQfT-tKt5KQ/eyedgvwd321oz5n/3-7%282%29.mp4"},
-    {title:"Episódio 6", thumb:"p.jpg", url:"https://download1510.mediafire.com/wxc0p43lc6ngYtnxqhLd3JkzizX3MOGNoDjn6x3xobFhaJwnGT177GE_EGCBOJEv6LamXETRC93eKTwSnlj1_YvOSZFxjR4Dp3SPAf8WL10nqV5K-aYyi2-gVIJjK6whB13MuxPSDhwk62YBh8q_DOOPIOc5mSuAd3yt2N-tb5VX95Q/a9dy27lhmljqq5c/3-8%282%29.mp4"},
-    {title:"Episódio 7", thumb:"p.jpg", url:"https://download1523.mediafire.com/zikx2uxzcjdgWC_odOgIe9VemQ3hzMxJO16awT6s1It08bCElR2g0WCQi1NBwb38_UEN5BjV0bUQJCZu59uQKlzVPFxlKIDOi48dVXAYsJKfqwo14G1OkoxxdlDeTDZ-UUXVZNWb8tQRgyBc6lJiexCcsrRcHwyM3o-PNrBNunnUsA/bg7t4er18557pgd/3-9%282%29.mp4"},
-    {title:"Episódio 8", thumb:"p.jpg", url:"https://download1073.mediafire.com/ehcso26anvog7sN3aGw9CmLUm5CT-a5aRCSWAqFo-PmI3DZ1HcZh1KDJHicZX5zYn_7SW5Rt25wkhluBrGYqCGYuXL9BohwwS8vnW9dHtFG_W0_w8CdePa_CY8vARK9wfqDXDfmJunnFKKTV4IdlAaHGN7R9ookAqddnIy4YlDOwkA/dievigvmg8rf39c/3-10%284%29.mp4"}
+    {title:"Episódio 1", thumb:"p.jpg", url:"https://download942.mediafire.com/pdwuqzxq5ndgXV9z7YQZGiGmmxTyESUzwWslkjOK89zQGr4McLqE1q0wnRtxm5D6aI-hNwbXhmk9iOqERMKx9dSfYa8Ixov3FvS6ZFiwUWnT5fF2v-i1LW_aGyidbk0PZOIHGZAC-tOVHZQORmirMWlmeJREEenQOdXU6FFrTi4tOg/nl5upvbbe45sr30/p2-the-walking-dead-dub-57794501temp3ep6_1.mp4"},
+    {title:"Episódio 2", thumb:"p.jpg", url:"https://download1528.mediafire.com/fmhkndg22fyg_CdzRNJMH_gKgudKlu3xEAMIaML5YRmAfk9RcMHCeEdgx8ano2L1MXKPDJtSxbHDN5_FQhC7VSSyNQPo8Lk2_jStcZnS3knPmQytHULq6Oj_dgAq0utopFvutZHaKPfRN3UooW8j3gCwzEbeFFI8oCcModALTnYbYg/72muqvo95wvr7ht/p2-the-walking-dead-dub-98974564temp3ep7_1.mp4"},
+    {title:"Episódio 3", thumb:"p.jpg", url:"#"},
+    {title:"Episódio 4", thumb:"p.jpg", url:"#"},
+    {title:"Episódio 5", thumb:"p.jpg", url:"#"},
+    {title:"Episódio 6", thumb:"p.jpg", url:"#"},
+    {title:"Episódio 7", thumb:"p.jpg", url:"#"},
+    {title:"Episódio 8", thumb:"p.jpg", url:"#"}
 ];
