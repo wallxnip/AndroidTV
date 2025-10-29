539b3425f2ee3d5303dce63d0f3f2ae316156b85
Apps <wallxnip@gmail.com>
2025-10-29 20:05:35 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,8 +1,8 @@
 // Dados de séries e episódios (8 episódios)
 const episodes = [
-    {title:"Episódio 1", thumb:"p.jpg", url:"https://download942.mediafire.com/pdwuqzxq5ndgXV9z7YQZGiGmmxTyESUzwWslkjOK89zQGr4McLqE1q0wnRtxm5D6aI-hNwbXhmk9iOqERMKx9dSfYa8Ixov3FvS6ZFiwUWnT5fF2v-i1LW_aGyidbk0PZOIHGZAC-tOVHZQORmirMWlmeJREEenQOdXU6FFrTi4tOg/nl5upvbbe45sr30/p2-the-walking-dead-dub-57794501temp3ep6_1.mp4"},
-    {title:"Episódio 2", thumb:"p.jpg", url:"https://download1528.mediafire.com/fmhkndg22fyg_CdzRNJMH_gKgudKlu3xEAMIaML5YRmAfk9RcMHCeEdgx8ano2L1MXKPDJtSxbHDN5_FQhC7VSSyNQPo8Lk2_jStcZnS3knPmQytHULq6Oj_dgAq0utopFvutZHaKPfRN3UooW8j3gCwzEbeFFI8oCcModALTnYbYg/72muqvo95wvr7ht/p2-the-walking-dead-dub-98974564temp3ep7_1.mp4"},
-    {title:"Episódio 3", thumb:"p.jpg", url:"#"},
+    {title:"Episódio 1", thumb:"p.jpg", url:"https://download1528.mediafire.com/u7sx3zc4dirgqM3QMlC8r6kKaNfpSn7Ih6Aj85YgQksBMrhwoH9WcaeeiuOSWUwbq64v0-pCSXmXyXVB1ZYBmgo8yoeO0--hezVco3W-AdEBnq8FDqfxDgOrX5BenweZWxjUFD6l5YJQLUYkKM4zSPdGgkwikFCOjqGlSC11qr7ibA/asss20v3j394mkl/p2-the-walking-dead-dub-03214785temp3ep8_1.mp4"},
+    {title:"Episódio 2", thumb:"p.jpg", url:"https://download1586.mediafire.com/d879do4vrihg9wtMUM9GWe6C1yWkegmIrxqG7SYqCE1oY8X7bo72Vm_z0PGk6lWAUK3kR7fuNpLK87PPW_WcdcvgXiyvCma6TVSR-lnqjkQ8GenlR1fq-TcAk7kHnrDanmmbQCL8KQt5JmVffJ0Si1Xr0u2l5JtrhWbV8IRiIO2UIA/vpqiwe90cfqh2bl/p2-the-walking-dead-dub-62047845temp3ep9_1.mp4"},
+    {title:"Episódio 3", thumb:"p.jpg", url:"https://download1336.mediafire.com/ueix33pn6pbg7LhYvyibVyw6zDGQnmdyuW67nJR7mWpGvk-rvbbW-a8bzHmpkreH0q2nZyAaXpvCSvzIyedzP9MKdoTtzsVORrg9zZhOoCdWTgAShtZdVr4WrSWNh5OUMHN1t9gYXRZ4Npui4BG3t2h8hw1-3em1OjVrshUFH2EB7A/iwgtudlaaluhwse/p2-the-walking-dead-dub-24765874temp3ep10_1.mp4"},
     {title:"Episódio 4", thumb:"p.jpg", url:"#"},
     {title:"Episódio 5", thumb:"p.jpg", url:"#"},
     {title:"Episódio 6", thumb:"p.jpg", url:"#"},
