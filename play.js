aecb3fd91ea6f810e2ff03325b922ab935797869
Apps <wallxnip@gmail.com>
2025-10-29 12:16:14 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,8 +1,8 @@
 // Dados de séries e episódios (8 episódios)
 const episodes = [
-    {title:"Episódio 1", thumb:"p.jpg", url:"1.mp4"},
-    {title:"Episódio 2", thumb:"p.jpg", url:"2.mp4"},
-    {title:"Episódio 3", thumb:"p.jpg", url:"https://download1476.mediafire.com/1k62dcn3nnggburseyTwTRUji4yT3L_D5obORNcekQLjqMPekspSkXZP3ZaGpupnnrNd8sUPpbMMljXT9-6e8jybF5KnP4oCDT3j5I-FSowIYl6etH_6RYQzmLPUMR8T2408Wgrf9wFUFkyZwSDnhchNvjbBarSCISidVVurdLsJWw/2y2yxu3x3rba1fr/3-5%287%29.mp4"},
+    {title:"Episódio 1", background:"bg1.jpg", thumb:"p.jpg", url:"1.mp4"},
+    {title:"Episódio 2", background:"bg1.jpg", thumb:"p.jpg", url:"2.mp4"},
+    {title:"Episódio 3", background:"bg1.jpg", thumb:"p.jpg", url:"https://download1476.mediafire.com/1k62dcn3nnggburseyTwTRUji4yT3L_D5obORNcekQLjqMPekspSkXZP3ZaGpupnnrNd8sUPpbMMljXT9-6e8jybF5KnP4oCDT3j5I-FSowIYl6etH_6RYQzmLPUMR8T2408Wgrf9wFUFkyZwSDnhchNvjbBarSCISidVVurdLsJWw/2y2yxu3x3rba1fr/3-5%287%29.mp4"},
     {title:"Episódio 4", thumb:"p.jpg", url:"https://download1580.mediafire.com/b8hqntihryigIC6SZMVzMZ_8GUjBSBuKTty05dIkY2LfwpsdFjwvUb65UqxJ0yyz9JzbchnJ2VAiXbAi_wq3roVNhbmXOus-ER3quXclgqPJwic8h4qcPk2uoIj_jm2AmZCstvQqNxit471iFXbeM2APkjPbnxFZmGQBD7LMFQE/l2b4zlk1w38xcf6/3-6%285%29.mp4"},
     {title:"Episódio 5", thumb:"p.jpg", url:"https://download1326.mediafire.com/umn26y7p8negENFjyCVMPL-rXGzti-2pxNfmjedHFfNLZmcbWbUtFu4j1-0Z91kHS5n-NNnGk8nyuKZdCqF1uJ7uXqbMcOnH24Py9umWwEd-LSk5a1xGdkczEETQlvcPAUW9w_x-tcsqfIZ1k3zZLUvnZHWSZhugMIyZQfT-tKt5KQ/eyedgvwd321oz5n/3-7%282%29.mp4"},
     {title:"Episódio 6", thumb:"p.jpg", url:"https://download1510.mediafire.com/wxc0p43lc6ngYtnxqhLd3JkzizX3MOGNoDjn6x3xobFhaJwnGT177GE_EGCBOJEv6LamXETRC93eKTwSnlj1_YvOSZFxjR4Dp3SPAf8WL10nqV5K-aYyi2-gVIJjK6whB13MuxPSDhwk62YBh8q_DOOPIOc5mSuAd3yt2N-tb5VX95Q/a9dy27lhmljqq5c/3-8%282%29.mp4"},
