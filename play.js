28a5e7dcee991206e94081c2f62d86ae16d5f3b1
Apps <wallxnip@gmail.com>
2025-10-30 11:08:34 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,11 +1,13 @@
 // Dados de séries e episódios (8 episódios)
 const episodes = [
-    {title:"Episódio 1", thumb:"p.jpg", url:"#"},
-    {title:"Episódio 2", thumb:"p.jpg", url:"#"},
-    {title:"Episódio 3", thumb:"p.jpg", url:"#"},
-    {title:"Episódio 4", thumb:"p.jpg", url:"#"},
-    {title:"Episódio 5", thumb:"p.jpg", url:"#"},
-    {title:"Episódio 6", thumb:"p.jpg", url:"#"},
-    {title:"Episódio 7", thumb:"p.jpg", url:"#"},
-    {title:"Episódio 8", thumb:"p.jpg", url:"#"}
+    {title:"Episódio 1", thumb:"https://twdserie.club/img/xa050ep8a7_p.jpg", url:"#"},
+    {title:"Episódio 2", thumb:"https://twdserie.club/img/p2xp62qi74_p.jpg", url:"#"},
+    {title:"Episódio 3", thumb:"https://twdserie.club/img/u5xq343qc7_p.jpg", url:"#"},
+    {title:"Episódio 4", thumb:"https://twdserie.club/img/e4rb6351vr_p.jpg", url:"#"},
+    {title:"Episódio 5", thumb:"https://twdserie.club/img/r7i6e3i46r_p.jpg", url:"#"},
+    {title:"Episódio 6", thumb:"https://twdserie.club/img/9s7xt1s96e_p.jpg", url:"#"},
+    {title:"Episódio 7", thumb:"https://twdserie.club/img/ji18d21oi1_p.jpg", url:"#"},
+    {title:"Episódio 8", thumb:"https://twdserie.club/img/3cx48g5yj4_p.jpg", url:"#"},
+
+
 ];
