d4c0739d7a0945877862f615464eb711d12077fa
Apps <wallxnip@gmail.com>
2025-11-01 14:12:07 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,6 +1,6 @@
 // Dados de séries e episódios (8 episódios)
 const episodes = [
-{title:"Episódio 1", thumb:"https://m.media-amazon.com/images/S/pv-target-images/933380a9bc886b5930d44036945008551632ba550be9539c5b1fddf2eac5be8c._BR-6_AC_SX480_FMwebp_.jpg", url:"https://tln-hg.top/stream/HpInBZMlMP3EtNB2orafwg/kjhhiuahiuhgihdf/1762059778/7109765/index-v1-a1.m3u8"},
+{title:"Episódio 1", thumb:"https://m.media-amazon.com/images/S/pv-target-images/933380a9bc886b5930d44036945008551632ba550be9539c5b1fddf2eac5be8c._BR-6_AC_SX480_FMwebp_.jpg", url:"https://oll238o.cloudatacdn.com/u5kjvwf5cxe3sdgge5bd4iy4j3bubwj4cv5hvmfkg5qpfymomrkayllgkdua/yzy1g0wv7a~V8hmfa3dY3?token=kemfssfbtufo3mn25fq4dxkl&expiry=1762017094835"},
 {title:"Episódio 2", thumb:"", url:""},
 {title:"Episódio 3", thumb:"", url:""},
 {title:"Episódio 4", thumb:"", url:""},
