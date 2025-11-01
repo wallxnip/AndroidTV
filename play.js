e163b5b3e08b602e8f36465a6ec3449d7e0de77b
Apps <wallxnip@gmail.com>
2025-11-01 14:05:23 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,13 +1,12 @@
 // Dados de séries e episódios (8 episódios)
 const episodes = [
-    {title:"Episódio 1", thumb:"https://twdserie.club/img/xa050ep8a7_p.jpg", url:"https://tln-earn.top/v/194xzgx0ziv7"},
-    {title:"Episódio 2", thumb:"https://twdserie.club/img/p2xp62qi74_p.jpg", url:"https://dsvplay.com/e/1scjivacl1y7"},
-    {title:"Episódio 3", thumb:"https://twdserie.club/img/u5xq343qc7_p.jpg", url:"https://tln-hg.top/stream/jPzyij8xvGVfbLD4l4zrlA/kjhhiuahiuhgihdf/1762021484/8233112/index-v1-a1.m3u8"},
-    {title:"Episódio 4", thumb:"https://twdserie.club/img/e4rb6351vr_p.jpg", url:"https://tln-hg.top/stream/_EZcyNR4h-3soUMOQEWvcg/kjhhiuahiuhgihdf/1762021609/8233197/index-v1-a1.m3u8"},
-    {title:"Episódio 5", thumb:"https://twdserie.club/img/r7i6e3i46r_p.jpg", url:"https://tln-hg.top/stream/gQ41RJNqhsieIOX5ZJ6OPQ/kjhhiuahiuhgihdf/1762021663/8233190/index-v1-a1.m3u8"},
-    {title:"Episódio 6", thumb:"https://twdserie.club/img/9s7xt1s96e_p.jpg", url:"https://tln-hg.top/stream/ILG1fsvp4jDu--sUDfw7JA/kjhhiuahiuhgihdf/1762021721/8233222/index-v1-a1.m3u8"},
-    {title:"Episódio 7", thumb:"https://twdserie.club/img/ji18d21oi1_p.jpg", url:"https://tln-hg.top/stream/t307VyC6udCIx1CXzBYpvw/kjhhiuahiuhgihdf/1762021758/8233264/index-v1-a1.m3u8"},
-    {title:"Episódio 8", thumb:"https://twdserie.club/img/3cx48g5yj4_p.jpg", url:"https://tln-hg.top/stream/XZ55dm7JNl9usCSGu1bBWQ/kjhhiuahiuhgihdf/1762021799/8233270/index-v1-a1.m3u8"},
-
+{title:"Episódio 1", thumb:"https://m.media-amazon.com/images/S/pv-target-images/933380a9bc886b5930d44036945008551632ba550be9539c5b1fddf2eac5be8c._BR-6_AC_SX480_FMwebp_.jpg", url:"https://tln-hg.top/stream/HpInBZMlMP3EtNB2orafwg/kjhhiuahiuhgihdf/1762059778/7109765/index-v1-a1.m3u8"},
+{title:"Episódio 2", thumb:"", url:""},
+{title:"Episódio 3", thumb:"", url:""},
+{title:"Episódio 4", thumb:"", url:""},
+{title:"Episódio 5", thumb:"", url:""},
+{title:"Episódio 6", thumb:"", url:""},
+{title:"Episódio 7", thumb:"", url:""},
+{title:"Episódio 8", thumb:"", url:""},
 
 ];
