92b8ff00063a387cda118dbb3d8733bcbc4b6585
Apps <wallxnip@gmail.com>
2025-11-01 15:05:05 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,12 +1,12 @@
 // Dados de séries e episódios (8 episódios)
 const episodes = [
-{title:"Episódio 1", audio:"https://radagoncp.xyz/m3/cTFONGJ5emVNUlAxK0JvUUdzMHErbzdPK0tZVm5MT1dMN0dpSXBacVU4Qk5YTmsvZ1J2cmpxMWJxaWtVYUFYa243RE91R0xSc0JlYlhpNXJ4eTVRbXAySWtlSVAyU2pzeFdPdEticjk0cmlSeEw5b2JEN1k3ejQySGoxY2ZiL09hdHZEa3BPeTJCVmVGQUg5TGVhOWViQW9SMzdXUHlKb0pTcVRpQXBJNEtiTE5WN1dMejFwR09hODdCUTdTd3N1TjMydUFJcHBKcHdsaXBtT2Y2b2QzL3FseXAwM0xleUFlTEk1ckJ2czBlWUowNWJiYVU2cmJzT3dnQXY3dllYOEdQaGVhL3hGMG16QXJ0VDRNbHArd3h3akRyMDVLR21lSFhDbWJjM0xaWEE9" thumb:"https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/a3ec4fe0-def2-4012-8ee6-e856ae45970d/9a66c226-a0d9-4797-be74-ccbd3dbc2fe5?host=wbd-images.prod-vod.h264.io&partner=beamcom&w=320", url:"https://radagoncp.xyz/m3/cTFONGJ5emVNUlAxK0JvUUdzMHErbzdPK0tZVm5MT1dMN0dpSXBacVU4Qk5YTmsvZ1J2cmpxMWJxaWtVYUFYa2tuZ3pZZkZUWGlEa2huZEhlZE1Wc0QweXFSUHE5S0ZQdXY2aWFaVUtyczBXd3FTTU5UQlBGY3hNOXVyaGE1bTM0QkYwdXFVajd4U21xTE9QV0xuN0JHRGl3dGNYM09uQ29lSnhGUGxkRkxqK1BFSDNRR0crTmE1YUI5bUprc0RZc2thV3dEM2trTlJJam9qTVg3VVJ4WUxqRG1Yd1gxeU0xR1IxK2xZZUo1UXdGNU1keTVaSGpRZU91Rll0eVJaQk8zSkU3RmdjSEl6blNqVUdLMHZ1SC9EYjFTdGZTNzRGMk85OHZHaSt2WXM9"},
-{title:"Episódio 2", audio:"" thumb:"", url:""},
-{title:"Episódio 3", audio:"" thumb:"", url:""},
-{title:"Episódio 4", audio:"" thumb:"", url:""},
-{title:"Episódio 5", audio:"" thumb:"", url:""},
-{title:"Episódio 6", audio:"" thumb:"", url:""},
-{title:"Episódio 7", audio:"" thumb:"", url:""},
-{title:"Episódio 8", audio:"" thumb:"", url:""},
+{title:"Episódio 1", thumb:"", url:""},
+{title:"Episódio 2", thumb:"", url:""},
+{title:"Episódio 3", thumb:"", url:""},
+{title:"Episódio 4", thumb:"", url:""},
+{title:"Episódio 5", thumb:"", url:""},
+{title:"Episódio 6", thumb:"", url:""},
+{title:"Episódio 7", thumb:"", url:""},
+{title:"Episódio 8", thumb:"", url:""},
 
 ];
