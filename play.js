a72bebbb24201842091e3f158aa4642424984082
Apps <wallxnip@gmail.com>
2025-11-02 18:16:05 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,12 +1,5 @@
 // Dados de séries e episódios (8 episódios)
 const episodes = [
-{title:"Episódio 1", thumb:"https://ei.phncdn.com/videos/202506/20/470639445/original/(m=qG9HTL0beaAaGwObaaaa)(mh=wB9nnaBewf2Axh5Q)0.jpg", url:"https://em-h.phncdn.com/hls/videos/202506/20/470639445/1080P_4000K_470639445.mp4/index-v1-a1.m3u8?validfrom=1762017314&validto=1762024514&ipa=1&hdl=-1&hash=YrSNAeFIaQoU%2FDSsl2tmt2jgKSA%3D"},
-{title:"Episódio 2", thumb:"https://cdn77-pic.xvideos-cdn.com/videos/thumbs169poster/06/d6/df/06d6dfdea56e3b8d7408ce6fcacfc9ba-1/06d6dfdea56e3b8d7408ce6fcacfc9ba.10.jpg", url:"https://cdn77-vid.xvideos-cdn.com/ESvlP-rhJ1sgURafMDH-5g==,1762031945/videos/hls/06/d6/df/06d6dfdea56e3b8d7408ce6fcacfc9ba-1/hls-1080p-041a3.m3u8"},
-{title:"Episódio 3", thumb:"https://cdn77-pic.xvideos-cdn.com/videos/thumbs169lll/ff/b8/44/ffb8447aeb504946783dcc696f393dfe/ffb8447aeb504946783dcc696f393dfe.30.jpg", url:"https://cdn77-vid.xvideos-cdn.com/JG2HzonqkBPlQ2Yk7J1rGA==,1762033040/videos/hls/ff/b8/44/ffb8447aeb504946783dcc696f393dfe/hls-360p.m3u8"},
-{title:"Episódio 4", thumb:"", url:"https://cdn77-vid.xvideos-cdn.com/Ybb9RKf1hLnXuQJ-cURc-g==,1762033094/videos/hls/85/8c/53/858c5343b4ae1d2cb3137d9ceb4df96d/hls-720p-f4b6c.m3u8"},
-{title:"Episódio 5", thumb:"https://tbi.sb-cd.com/t/13058407/1/3/w:300/t6-enh/hot-ebony-milf-give-me.jpg", url:"https://vdownload-48.sb-cd.com/1/3/13058407-1080p.mp4?secure=InXFGInwfzCCctiSTV-o0w,1762065958&m=48&d=1&_tid=13058407"},
-{title:"Episódio 6", thumb:"", url:""},
-{title:"Episódio 7", thumb:"", url:""},
-{title:"Episódio 8", thumb:"", url:""},
+{title:"Episódio 1", thumb:"https://image.tmdb.org/t/p/original/3ynZzFOTXVKgN9zaIXLehZCKKfY.jpg", url:"https://download848.mediafire.com/s8mf2fuxd0kg0bfVUKFoeblbSAPtfbO9YPPZnimSZzDCweKqLSn9MtJFYfHfbeSzC77cdPk2Qqh4dNfLWEzVpPOBZWMluvSRfkZhirx5O5Q5to3kywwMr-yVjuzEHJTbCjy3pllX4Tm41KWa9JQSIcWErpe56FeuVIKJR-FLiSsT/w5b3mccxi5gqga5/4-8%282%29.mp4"},
 
 ];
