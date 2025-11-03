207ef245bb39aade50396ead3e02c65c34167ee4
Apps <wallxnip@gmail.com>
2025-11-03 18:06:15 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,5 +1,5 @@
 // Dados de séries e episódios (8 episódios)
 const episodes = [
-{title:"Episódio 1", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://www.mediafire.com/file/qu1bs8hetxox473/TDO01.mp4/file"},
-
+{title:"Episódio 1", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://www.mediafire.com/file/qu1bs8hetxox473/TDO01.mp4"},
+{title:"Episódio 1", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://download2388.mediafire.com/51t15osudurg0_5g9v4N5o1XmuXzB1JOcz6aEhgvtXclauSsMECi_C7mVTknstYvTcXbIeOJhlnh8jnkAsHA8axGhcccMpjsMwKDEfzEO6s-oV0mzNMlJKpiIPf6bZewfwhFB4rFi1-iQ9jnnSVAzYfw_tmW52mFiSx4SCnU_h27KIo/qu1bs8hetxox473/TDO01.mp4"},
 ];
