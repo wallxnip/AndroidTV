9ccc78d02184274e96b740e6f9680a9211d58f44
Apps <wallxnip@gmail.com>
2025-11-04 04:51:16 -0300

    Atualizar o kmplay001.js

--- kmplay001.js
+++ kmplay001.js
@@ -3,11 +3,11 @@
 // ===========================
 // Mudar poster do vídeo
 const video = document.getElementById('player');
-video.poster = 'nova_capa.jpg';
+video.poster = 'https://image.tmdb.org/t/p/original/6eGhkMobNuL3sh9KSpfhxBSryHh.jpg';
 
 // Mudar logo
 const logo = document.getElementById('topLogo');
-logo.src = 'nova_logo.png';
+logo.src = 'https://image.tmdb.org/t/p/original/3hRYalezTWUScmsiBq9L2f0QHJe.png';
          
 const episodes = [
 { title:"Episódio 1", url:"https://www.mediafire.com/file/qu1bs8hetxox473/TDO01.html", thumb:"#" },
