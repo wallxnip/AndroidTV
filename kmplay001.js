6f37311657cd930080a78f0330023376dadedfe3
Apps <wallxnip@gmail.com>
2025-11-04 07:42:49 -0300

    Atualizar o kmplay001.js

--- kmplay001.js
+++ kmplay001.js
@@ -5,9 +5,9 @@ const config = {
   poster: "https://image.tmdb.org/t/p/original/14g5B3sqDLH6ldDBJLcRBvzh2kc.jpg",
   logo: "https://image.tmdb.org/t/p/original/tQsNL1ULG7NsE6Hrzxtg2hFNIxA.png",
   episodes: [
-    { title: "Episódio 1", url: "https://www.mediafire.com/file/yzf5x2xozw0znuv/TH01.mkv", thumb: "https://i.postimg.cc/pTqKR8Y2/cover.webp" },
-    { title: "Episódio 2", url: "https://www.mediafire.com/file/tfzr1kndokyvny1/TH01.mp4", thumb: "#" },
-    { title: "Episódio 3", url: "#", thumb: "#" },
+    { title: "Episódio 1", url: "https://www.mediafire.com/file/tfzr1kndokyvny1/TH01.mp4", thumb: "https://is1-ssl.mzstatic.com/image/thumb/OlDsjMI8DkoEkaUXgnaMwA/1920x1080.webp" },
+    { title: "Episódio 2", url: "https://www.mediafire.com/file/77o8u2yoh4wa64t/TH02.mp4/file", thumb: "https://is1-ssl.mzstatic.com/image/thumb/Q6xG679rsvztbi3_rrQ0fA/1920x1080.webp" },
+  
   ]
 };
 
