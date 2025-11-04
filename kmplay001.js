65639d7e31e314d162a47dbdea1edf187a007ad4
Apps <wallxnip@gmail.com>
2025-11-04 07:09:05 -0300

    Atualizar o kmplay001.js

--- kmplay001.js
+++ kmplay001.js
@@ -2,8 +2,8 @@
 // Configuração dinâmica
 // ===========================
 const config = {
-  poster: "https://i.postimg.cc/pTqKR8Y2/cover.webp",
-  logo: "https://i.postimg.cc/sXV5kKSj/logof.webp",
+  poster: "https://image.tmdb.org/t/p/original/14g5B3sqDLH6ldDBJLcRBvzh2kc.jpg",
+  logo: "https://image.tmdb.org/t/p/original/tQsNL1ULG7NsE6Hrzxtg2hFNIxA.png",
   episodes: [
     { title: "Episódio 1", url: "https://www.mediafire.com/file/yzf5x2xozw0znuv/TH01.mkv", thumb: "https://i.postimg.cc/pTqKR8Y2/cover.webp" },
     { title: "Episódio 2", url: "https://www.mediafire.com/file/tfzr1kndokyvny1/TH01.mp4", thumb: "#" },
