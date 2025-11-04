4982eb4d2e3b2d46bae2e9d311f71e66639087c4
Apps <wallxnip@gmail.com>
2025-11-04 04:44:31 -0300

    Criar kmplay001.js

---
+++ kmplay001.js
@@ -0,0 +1,17 @@
+
+    // ===========================
+    // Lista de episódios
+    // ===========================
+    // Mudar poster do vídeo
+        const video = document.getElementById('player');
+        video.poster = 'nova_capa.jpg';
+
+      // Mudar logo
+         const logo = document.getElementById('topLogo');
+         logo.src = 'nova_logo.png';
+         
+    const episodes = [
+      { title:"Episódio 1", url:"#", thumb:"#" },
+      { title:"Episódio 2", url:"#", thumb:"#" },
+      { title:"Episódio 3", url:"#", thumb:"#" },
+    ];
