5cd9968df32d775719a015d81bfc49a4c2f692a1
Apps <wallxnip@gmail.com>
2025-11-04 04:49:15 -0300

    Atualizar o kmplay001.js

--- kmplay001.js
+++ kmplay001.js
@@ -1,17 +1,22 @@
+// ===========================
+// Lista de episódios
+// ===========================
+// Mudar poster do vídeo
+const video = document.getElementById('player');
+video.poster = 'nova_capa.jpg';
 
-    // ===========================
-    // Lista de episódios
-    // ===========================
-    // Mudar poster do vídeo
-        const video = document.getElementById('player');
-        video.poster = 'nova_capa.jpg';
-
-      // Mudar logo
-         const logo = document.getElementById('topLogo');
-         logo.src = 'nova_logo.png';
+// Mudar logo
+const logo = document.getElementById('topLogo');
+logo.src = 'nova_logo.png';
          
-    const episodes = [
-      { title:"Episódio 1", url:"https://www.mediafire.com/file/qu1bs8hetxox473/TDO01.html", thumb:"#" },
-      { title:"Episódio 2", url:"#", thumb:"#" },
-      { title:"Episódio 3", url:"#", thumb:"#" },
-    ];
+const episodes = [
+{ title:"Episódio 1", url:"https://www.mediafire.com/file/qu1bs8hetxox473/TDO01.html", thumb:"#" },
+{ title:"Episódio 2", url:"#", thumb:"#" },
+{ title:"Episódio 3", url:"#", thumb:"#" },
+
+
+
+
+
+
+];
