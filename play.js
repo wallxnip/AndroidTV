f0d999feb361bc2def5fe693f1af916b52d30606
Apps <wallxnip@gmail.com>
2025-11-20 19:09:40 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,9 +1,9 @@
 // Dados de séries e episódios
 const episodes = [
-{title:"Episódio 1", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1102-t.vmeas.cloud/hls/xqx2okfaynokjiqbteqc572d4fz472vvgtsf6c33m4b6uepftga2q5g6y3lq/index-v1-a1.m3u8"},
-{title:"Episódio 2", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1400-p10.vmeas.cloud/hls/xqx2opnaynokjiqbteqc5l2mudqashuqur2koowqoouy3k4bhen44v3prsoq/index-v1-a1.m3u8"},
-{title:"Episódio 3", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1086-u.vmeas.cloud/hls/xqx2px5aynokjiqbteqc5jizut3hqjkmjkdrmxckomakcuankqu5uzrqxxwq/index-v1-a1.m3u8"},
-{title:"Episódio 4", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1034-f2.vmeas.cloud/hls/xqx2pm5bynokjiqbteqc57cnxwf4pudhlevuvfcl5ye7eqaej3ylecauxida/index-v1-a1.m3u8"},
+{title:"Episódio 1", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1181-pd.vmwesa.online/hls/xqx2oynbynokjiqbteqc5lq7xa35yu7cfeqlxzq7iyx2gixy3ch5i3ygitaa/index-v1-a1.m3u8"},
+{title:"Episódio 2", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1100-r.vmeas.cloud/hls/xqx2pc5bynokjiqbteqc5ky4xzcbbjne2qvwltzwmho4bouyvjmyjihicywa/index-v1-a1.m3u8"},
+{title:"Episódio 3", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1245-w.vmwesa.online/hls/xqx2o6fbynokjiqbteqc5isn44v5c5pirtyg5fo33l2ahnigtrgkeahdhyca/index-v1-a1.m3u8"},
+{title:"Episódio 4", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1158-u.vmeas.cloud/hls/xqx2osvbynokjiqbteqc5pqgwcuijahb2euczwowtpvuknsvqy334mnt7ffa/index-v1-a1.m3u8"},
 
 
 ];
