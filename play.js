c686dd5f3487074e8cb0f976e9aac2c5c1f22ac8
Apps <wallxnip@gmail.com>
2025-11-20 15:11:34 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,9 +1,9 @@
 // Dados de séries e episódios
 const episodes = [
-{title:"Episódio 1", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:""},
-{title:"Episódio 2", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:""},
-{title:"Episódio 3", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:""},
-{title:"Episódio 4", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:""},
+{title:"Episódio 1", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1102-t.vmeas.cloud/hls/xqx2okfaynokjiqbteqc572d4fz472vvgtsf6c33m4b6uepftga2q5g6y3lq/index-v1-a1.m3u8"},
+{title:"Episódio 2", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1400-p10.vmeas.cloud/hls/xqx2opnaynokjiqbteqc5l2mudqashuqur2koowqoouy3k4bhen44v3prsoq/index-v1-a1.m3u8"},
+{title:"Episódio 3", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1086-u.vmeas.cloud/hls/xqx2px5aynokjiqbteqc5jizut3hqjkmjkdrmxckomakcuankqu5uzrqxxwq/index-v1-a1.m3u8"},
+{title:"Episódio 4", thumb:"https://image.tmdb.org/t/p/original/AjwsOW62iFDUAx9ehJYoPdHC5h8.jpg", url:"https://box-1034-f2.vmeas.cloud/hls/xqx2pm5bynokjiqbteqc57cnxwf4pudhlevuvfcl5ye7eqaej3ylecauxida/index-v1-a1.m3u8"},
 
 
 ];
