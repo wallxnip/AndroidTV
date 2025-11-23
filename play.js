d9d39f5c8cdef7affb0422ffed50438dee3172f5
Apps <wallxnip@gmail.com>
2025-11-23 20:34:47 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,9 +1,9 @@
 // Dados de séries e episódios
 const episodes = [
-{title:"Episódio 1", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-c-o-l-8.vmeas.cloud/v/01/02082/wra3nkgcxw4w_n/The%20Walking%20Dead_S07E11_.mp4?t=XPkyX4YFGfIbzcIYS6J9FBu4Enw4OiA_McNQck_WnGw&s=1763939821&e=43200&v=&sp=220&i=0.0"},
-{title:"Episódio 2", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-1100-r.vmeas.cloud/hls/xqx2pc5bynokjiqbteqc5ky4xzcbbjne2qvwltzwmho4bouyvjmyjihicywa/index-v1-a1.m3u8"},
-{title:"Episódio 3", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-1245-w.vmwesa.online/hls/xqx2o6fbynokjiqbteqc5isn44v5c5pirtyg5fo33l2ahnigtrgkeahdhyca/index-v1-a1.m3u8"},
-{title:"Episódio 4", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-1158-u.vmeas.cloud/hls/xqx2osvbynokjiqbteqc5pqgwcuijahb2euczwowtpvuknsvqy334mnt7ffa/index-v1-a1.m3u8"},
+{title:"Episódio 1", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-1249-o.vmwesa.online/hls/xqx2o3nbynokjiqbteqc5lcb5evczhthnvjewk564qsabdxifregaltjftaa/index-v1-a1.m3u8"},
+{title:"Episódio 2", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-1183-rp.vmwesa.online/hls/xqx2pcnbynokjiqbteqc5iy2wwt52pfrgmn4lvll2fedgcf5x6pfoytkjcsa/index-v1-a1.m3u8"},
+{title:"Episódio 3", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-1093-w.vmeas.cloud/hls/xqx2ok5bynokjiqbteqc5jit4wix5kygdp5yfycxppaci7nrb54h4xplwgfq/index-v1-a1.m3u8"},
+{title:"Episódio 4", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-1087-y.vmeas.cloud/hls/xqx2pw5bynokjiqbteqc57ao5gi5bgmzpme2aymjfri3oonnjull45n3hg6a/index-v1-a1.m3u8"},
 
 
 ];
