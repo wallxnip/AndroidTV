aab090fca0fb99e9773f1268767de09cc9dd9aae
Apps <wallxnip@gmail.com>
2025-12-06 01:38:41 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,9 +1,7 @@
 // Dados de séries e episódios
 const episodes = [
 {title:"Episódio 1", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://download1338.mediafire.com/did5aoxiywogDZX7MT_vNzviV1i2liZ2j8LoLTNr-Hf4ltFTYZAfCXyxNT7SqU-WIcT10j_DLi4J6F7DVCqKgFgWVRtIFSEr-XskHDUmXmI82QbosMxM-7O4XdzGARfNn2XelhAymWSw5lu4xPg5YA0qaVWQ9Mjzbl0IdSv-IBXEWg/6b0xhks54n1p2lh/p2-the-walking-dead-dub-66587804temp7ep16_1.mp4"},
-{title:"Episódio 2", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://download937.mediafire.com/dt2axkvjvl1gS35_3PZ9FvWENNo1UV3pDAGCJCkrpmmvTsQsYyALZ1zMvNRn4lak1TwaKh2HfkBXymk8SYPZfZUhK_xN_zV4dn6xnPPZvFrSw19F4n64X7-cLKDKZDX95oo-VlotvXWXlBTFCYwvo9NAmYU3TVsc__Sfn-00gJj7DA/ws2csultz4z7v6k/8-1%283%29.mp4"},
-{title:"Episódio 3", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://download937.mediafire.com/dt2axkvjvl1gS35_3PZ9FvWENNo1UV3pDAGCJCkrpmmvTsQsYyALZ1zMvNRn4lak1TwaKh2HfkBXymk8SYPZfZUhK_xN_zV4dn6xnPPZvFrSw19F4n64X7-cLKDKZDX95oo-VlotvXWXlBTFCYwvo9NAmYU3TVsc__Sfn-00gJj7DA/ws2csultz4z7v6k/8-1%283%29.mp4"},
-{title:"Episódio 4", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://download1511.mediafire.com/g2qowsmmccwgWo9eOzVEM8BOik2o5ZjTEVsnljMdUI1yuCuoOOp_uUPoE4BeP1d2ZlsZAvzks8jKscxKh24gqN2uHspBUhyaABgmdXNfFZY0PWieXg-xq0ZoY0uqqZm2ZATbivTBgbg56urW4nHGI5HtdL1KxyFI9C5N0trZSoX3QQ/wybxcj3bsa4h0zo/8-3%282%29.mp4"},
+{title:"Episódio 2", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://download937.mediafire.com/dt2axkvjvl1gS35_3PZ9FvWENNo1UV3pDAGCJCkrpmmvTsQsYyALZ1zMvNRn4lak1TwaKh2HfkBXymk8SYPZfZUhK_xN_zV4dn6xnPPZvFrSw19F4n64X7-cLKDKZDX95oo-VlotvXWXlBTFCYwvo9NAmYU3TVsc__Sfn-00gJj7DA/ws2csultz4z7v6k/8-1%283%29.mp4"}
 
 
 ];
