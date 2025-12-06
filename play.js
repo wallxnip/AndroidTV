ed58fac717c0e6c5b0678596d7db3bfbc6d0ade7
Apps <wallxnip@gmail.com>
2025-12-06 01:28:30 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,9 +1,9 @@
 // Dados de séries e episódios
 const episodes = [
-{title:"Episódio 1", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-1249-o.vmwesa.online/hls/xqx2o3nbynokjiqbteqc5lcb5evczhthnvjewk564qsabdxifregaltjftaa/index-v1-a1.m3u8"},
-{title:"Episódio 2", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-1183-rp.vmwesa.online/hls/xqx2pcnbynokjiqbteqc5iy2wwt52pfrgmn4lvll2fedgcf5x6pfoytkjcsa/index-v1-a1.m3u8"},
-{title:"Episódio 3", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-1093-w.vmeas.cloud/hls/xqx2ok5bynokjiqbteqc5jit4wix5kygdp5yfycxppaci7nrb54h4xplwgfq/index-v1-a1.m3u8"},
-{title:"Episódio 4", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://box-1087-y.vmeas.cloud/hls/xqx2pw5bynokjiqbteqc57ao5gi5bgmzpme2aymjfri3oonnjull45n3hg6a/index-v1-a1.m3u8"},
+{title:"Episódio 1", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://download1652.mediafire.com/gd7uvdm25kzg3Ytzqs5F2yKiFEUfKNRgdRHeGeo0UUWRETqqnH5zWOVHw1JocY76D-2DtpLaaUVQSFeo5yMhLwftIOV4KIGdeHRiJhjtYNhnnVuhdwqNHMTOgQlfMckkoM4tQxAMcdt-S0rfv-IMC3X74mcI_SVvQPD4WWIzGlsZ/c1xtbkj6m7zlkow/7-16.mp4"},
+{title:"Episódio 2", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://download937.mediafire.com/dt2axkvjvl1gS35_3PZ9FvWENNo1UV3pDAGCJCkrpmmvTsQsYyALZ1zMvNRn4lak1TwaKh2HfkBXymk8SYPZfZUhK_xN_zV4dn6xnPPZvFrSw19F4n64X7-cLKDKZDX95oo-VlotvXWXlBTFCYwvo9NAmYU3TVsc__Sfn-00gJj7DA/ws2csultz4z7v6k/8-1%283%29.mp4"},
+{title:"Episódio 3", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://download937.mediafire.com/dt2axkvjvl1gS35_3PZ9FvWENNo1UV3pDAGCJCkrpmmvTsQsYyALZ1zMvNRn4lak1TwaKh2HfkBXymk8SYPZfZUhK_xN_zV4dn6xnPPZvFrSw19F4n64X7-cLKDKZDX95oo-VlotvXWXlBTFCYwvo9NAmYU3TVsc__Sfn-00gJj7DA/ws2csultz4z7v6k/8-1%283%29.mp4"},
+{title:"Episódio 4", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://download1511.mediafire.com/g2qowsmmccwgWo9eOzVEM8BOik2o5ZjTEVsnljMdUI1yuCuoOOp_uUPoE4BeP1d2ZlsZAvzks8jKscxKh24gqN2uHspBUhyaABgmdXNfFZY0PWieXg-xq0ZoY0uqqZm2ZATbivTBgbg56urW4nHGI5HtdL1KxyFI9C5N0trZSoX3QQ/wybxcj3bsa4h0zo/8-3%282%29.mp4"},
 
 
 ];
