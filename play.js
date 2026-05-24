8d1c1b0ed56a8c77f7f8a0eecc67e97591fc8835
Apps <wallxnip@gmail.com>
2026-05-23 21:06:16 -0300

    Atualizar o play.js

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 // Dados de séries e episódios
 const episodes = [
-{title:"Episódio 1", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://download1338.mediafire.com/did5aoxiywogDZX7MT_vNzviV1i2liZ2j8LoLTNr-Hf4ltFTYZAfCXyxNT7SqU-WIcT10j_DLi4J6F7DVCqKgFgWVRtIFSEr-XskHDUmXmI82QbosMxM-7O4XdzGARfNn2XelhAymWSw5lu4xPg5YA0qaVWQ9Mjzbl0IdSv-IBXEWg/6b0xhks54n1p2lh/p2-the-walking-dead-dub-66587804temp7ep16_1.mp4"},
-{title:"Episódio 2", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://download937.mediafire.com/dt2axkvjvl1gS35_3PZ9FvWENNo1UV3pDAGCJCkrpmmvTsQsYyALZ1zMvNRn4lak1TwaKh2HfkBXymk8SYPZfZUhK_xN_zV4dn6xnPPZvFrSw19F4n64X7-cLKDKZDX95oo-VlotvXWXlBTFCYwvo9NAmYU3TVsc__Sfn-00gJj7DA/ws2csultz4z7v6k/8-1%283%29.mp4"}
+{title:"Episódio 1", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://video.wixstatic.com/video/8d3786_251e5a43d9384b21ab6525a558d1d830/720p/mp4/file.mp4"},
+{title:"Episódio 2", thumb:"https://images7.alphacoders.com/565/565053.jpg", url:"https://video.wixstatic.com/video/8d3786_dc87babb750b4600ab81da3ed110c8aa/720p/mp4/file.mp4"}
 
 
 ];
